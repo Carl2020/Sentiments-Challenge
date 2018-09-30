eb0ffaead6c0358b5b191078f53f6a9ffbe6a24b
Mr Blunt <carl.adams97@gmail.com>
2018-09-30 16:02:07 -0500

    Finished coding to run vadar sentiment analyis on the last 100 tweets of 5 major media sources.

---
+++ News_Mood/.gitignore/config.py
@@ -0,0 +1,11 @@
+
+
+consumer_key = "IlJjFot08tnReqoOoC9iB1g2w"
+
+consumer_secret = "9GJntSI4rwNa9i2z3h0UjRKfnmBQItQlE35E6Knpc8AxD1imhC"
+
+access_token = "1033343127120211969-sLp7R8W61G2fk1JlECUIJ7mQ7LRrEg"
+
+access_token_secret = "uLe9vDyJ8n8d5VT4hTTsoUh2BEtaI3lZxyq1GonfLRxbn"
+
+
